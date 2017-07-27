e5a8d27ef4cc46f6cbfe11aa527d788c56e4fa38
hypervisor <hypervisor@DESKTOP-8RMNN45>
2017-07-27 22:53:10 +0800

    --o2AuthSecuritySpringboot initial commit

---
+++ src/test/java/org/rvslab/chapter2/ApplicationTests.java
@@ -0,0 +1,67 @@
+package org.rvslab.chapter2;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebIntegrationTest
+
+public class ApplicationTests {
+	@Autowired
+	Environment env;
+/*
+	@Test
+	public void testVanillaService() {
+		RestTemplate restTemplate = new RestTemplate();
+		Greet greet = restTemplate.getForObject("http://localhost:" + env.getProperty("server.port"), Greet.class);
+		Assert.assertEquals("Hello World!", greet.getMessage());
+	}
+
+	@Test
+	public void testSecureService() {
+		//System.out.println("pw: "+env.getProperty("security.user.password"));
+		String plainCreds = "guest:guest123";
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Basic " + new String(Base64.encode(plainCreds.getBytes())));
+		HttpEntity<String> request = new HttpEntity<String>(headers);
+		RestTemplate restTemplate = new RestTemplate();
+
+		ResponseEntity<Greet> response = restTemplate.exchange("http://localhost:9090", HttpMethod.GET, request,
+				Greet.class);
+		Assert.assertEquals("Hello World!", response.getBody().getMessage());
+	}
+	*/
+	 @Test
+	  public void testOAuthService() {
+	        ResourceOwnerPasswordResourceDetails resource = new ResourceOwnerPasswordResourceDetails();
+	        resource.setUsername("guest");
+	        resource.setPassword("guest123");
+	          resource.setAccessTokenUri("http://localhost:9090/oauth/token");
+	        resource.setClientId("trustedclient");
+	        resource.setClientSecret("trustedclient123");
+	        resource.setGrantType("password");
+	  
+	        DefaultOAuth2ClientContext clientContext = new DefaultOAuth2ClientContext();
+	        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resource, clientContext);
+	 
+	        Greet greet = restTemplate.getForObject("http://localhost:9090", Greet.class);
+
+	        Assert.assertEquals("Hello World!", greet.getMessage());
+	  }
+}
